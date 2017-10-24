5ca63b450be75827244d4adaea1e7f60681ff5a0
vbalasub <vbalasub@redhat.com>
2017-10-24 15:51:40 -0500

    Second commit

--- src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -18,14 +18,14 @@ package org.jboss.as.quickstarts.helloworld;
 
 /**
  * A simple CDI service which is able to say hello to someone
- * 
+ *
  * @author Pete Muir
- * 
+ *
  */
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "Hello " + name + "!";
+        return "Hola " + name + "!";
     }
 
 }
